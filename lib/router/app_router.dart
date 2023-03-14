d937f5f9c448651149f70d9eca783950c1b9d4ab
gcode_rgonzalez <rgonzalez@geniuscode.mx>
2023-03-13 18:42:52 -0700

    [+] se resuelven los cambios en las rutas

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -10,7 +10,6 @@ class AppRouter {
 
   //DEFINICION DEL MENU DE LA APP
   static final menuOptions   = <MenuOption>[    
-<<<<<<< HEAD
     MenuOption(route: 'login',      icon: Icons.logout_outlined,        nombre:'Login',         screen:  Login()),
     MenuOption(route: 'home',       icon: Icons.home_outlined,          nombre:'Home',          screen:  HomeScreen()),
     MenuOption(route: 'recargas',   icon: Icons.phone_iphone_outlined,  nombre:'Recargas',      screen: const ListaScreen()),
@@ -18,14 +17,6 @@ class AppRouter {
     MenuOption(route: '',           icon: null,      nombre:null,                               screen: Expanded(child: Container())),
     MenuOption(route: 'lista3',     icon: Icons.settings_outlined,      nombre:'Ajustes',       screen: const Programas()),
     MenuOption(route: 'login',      icon: Icons.logout_outlined,        nombre:'Cerrar Sesion', screen:  Login()),
-=======
-    MenuOption(route: 'login',  icon: Icons.credit_card,  nombre:'Login',          screen:  Login()),
-    MenuOption(route: 'home',   icon: Icons.list,         nombre:'Home',           screen: const HomeScreen()),
-    MenuOption(route: 'lista',  icon: Icons.list,         nombre:'Lista',          screen: const ListaScreen()),
-    MenuOption(route: 'lista2', icon: Icons.list,         nombre:'Lista 2',        screen: const Lista2Screen()),
-    MenuOption(route: 'lista3', icon: Icons.list,         nombre:'Programas',      screen: const Programas()),    
-    MenuOption(route: 'config', icon: Icons.list,         nombre:'configuracion',  screen: const IndexConfiguracion()),    
->>>>>>> a4e00f9d87216a6b6262cb9de657b3287153a6ba
     
   ];
 
